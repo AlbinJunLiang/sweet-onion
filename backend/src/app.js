0490321409b37ac273cb9c03b43f6df169f733c3
ALBIN.LIANG <ALBIN.LIANG@.ucr.ac.cr>
2026-06-07 01:59:53 -0600

    fix: all

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,6 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"] // Asegúrate de incluir Authorization
 }));
 
-app.options('*', cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/completions", completionRoutes);
